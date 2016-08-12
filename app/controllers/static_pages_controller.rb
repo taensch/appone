35cad2dfa60f21051f87b8c2b5e05877d6b0f10a
Tanja <savanin.tanja@gmail.com>
2016-08-12 16:54:48 +0200

    4.5 styled landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,8 @@
 class StaticPagesController < ApplicationController
   def index
   end
-end
-
-
-class StaticPagesController < ApplicationController
 
   def landing_page
-    @featured_product = Product.first
+	@products = Product.limit(3)	
   end
-
 end
-
