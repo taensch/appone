9c3d47539bc37c63373f5f136844786eea797233
Tanja <savanin.tanja@gmail.com>
2016-08-23 18:17:51 +0200

    5.10 add user admin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,8 +16,12 @@ class CommentsController < ApplicationController
     end
   end
 
- def destroy
- end
+def destroy
+  @comment = Comment.find(params[:id])
+  product = @comment.product
+  @comment.destroy
+  redirect_to product
+end
     
  private
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,9 +3,12 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
-
 end
 
 # class Ability
