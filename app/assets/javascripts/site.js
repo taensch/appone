99f8f9985381a951ffa253abb634ccc1ec30c79f
Tanja <savanin.tanja@gmail.com>
2016-09-19 22:51:39 +0200

    6.10 implemented AJAX for comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,28 +1,81 @@
-document.addEventListener("turbolinks:load", function() {
-   // raty
-    $('.rating').raty({
-        path: '/assets',
-        scoreName: 'comment[rating]'
-    });
-    $('.rated').raty({
-        path: '/assets',
+
+// After adding AJAX, restucture loading
+// document.addEventListener("turbolinks:load", function() {
+//    // raty
+//     $('.rating').raty({
+//         path: '/assets',
+//         scoreName: 'comment[rating]'
+//     });
+//     $('.rated').raty({
+//         path: '/assets',
+//         readOnly: true,
+//         score: function() {
+//             return $(this).attr('data-score');
+//         }
+//     });
+
+//    // elevateZoom
+//       $('.img-zoom').elevateZoom({
+//         zoomType: "lens",
+//         lensShape: "round",
+//         lensSize: 300,
+//           scrollZoom: true,
+//                 lensFadeIn: 500,
+//                 lensFadeOut: 500
+//       });
+// })
+
+// After adding AJAX, restucture loading
+// document.addEventListener("turbolinks:load", function() {
+//    // raty
+//     $('.rating').raty({
+//         path: '/assets',
+//         scoreName: 'comment[rating]'
+//     });
+//     $('.rated').raty({
+//         path: '/assets',
+//         readOnly: true,
+//         score: function() {
+//             return $(this).attr('data-score');
+//         }
+//     });
+
+//    // elevateZoom
+//       $('.img-zoom').elevateZoom({
+//         zoomType: "lens",
+//         lensShape: "round",
+//         lensSize: 300,
+//           scrollZoom: true,
+//                 lensFadeIn: 500,
+//                 lensFadeOut: 500
+//       });
+// })
+
+
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
         readOnly: true,
         score: function() {
             return $(this).attr('data-score');
         }
     });
+};
 
+$(document).on('turbolinks:load', function() {
+ 
+  refreshRating(); 
+  // ajaxSuccess didnt work
+   
    // elevateZoom
-      $('.img-zoom').elevateZoom({
+    $('.img-zoom').elevateZoom({
         zoomType: "lens",
         lensShape: "round",
         lensSize: 300,
-          scrollZoom: true,
-                lensFadeIn: 500,
-                lensFadeOut: 500
+        scrollZoom: true,
+              lensFadeIn: 500,
+              lensFadeOut: 500
       });
-})
-
-
+});
 
 
